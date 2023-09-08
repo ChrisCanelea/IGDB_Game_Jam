eb79c01ce030ab4b6bee029dce101ab6d15853d5
Chris <chris.canelea@mail.utoronto.ca>
2023-09-08 14:49:55 -0400

    stage function prototypes

--- classes/stage.cpp
+++ classes/stage.cpp
@@ -8,5 +8,218 @@
 Stage::Stage() // Default constructor
 {
     this->sprite = LoadTexture("assets/background.png");
-    // this->playArea = 
+    this->playArea = {-250, -250, 500, 500};
+    this->playerReference = NULL;
+    this->northWall = {getPlayArea().x - (getPlayArea().width/2), getPlayArea().y - getPlayArea().height, getPlayArea().width * 2, getPlayArea().height};
+    this->eastWall = {getPlayArea().x + getPlayArea().width, getPlayArea().y, getPlayArea().width/2, getPlayArea().height};
+    this->southWall = {getPlayArea().x - (getPlayArea().width/2), getPlayArea().y + getPlayArea().height, getPlayArea().width * 2, getPlayArea().height};
+    this->westWall = {getPlayArea().x - (getPlayArea().width/2), getPlayArea().y, getPlayArea().width/2, getPlayArea().height};
+    this->maxEnemies = 10;
+    this->maxProjectiles = 10;
+    this->initialEnemies = 3;
+    this->enemyRespawnTime = 600;
+    this->projectileRespawnTime = 600;
+    this->exitLocation = generateExitPosition();
+    this->enemiesArray = createEnemyArray();
+    this->projectilesArray = createProjectileArray();
+}
+
+Stage::Stage(Texture2D sprite_, Rectangle playArea_, Player* playerReference_) 
+{
+    this->sprite = sprite_;
+    this->playArea = playArea_;
+    this->playerReference = playerReference_;
+    this->northWall = {getPlayArea().x - (getPlayArea().width/2), getPlayArea().y - getPlayArea().height, getPlayArea().width * 2, getPlayArea().height};
+    this->eastWall = {getPlayArea().x + getPlayArea().width, getPlayArea().y, getPlayArea().width/2, getPlayArea().height};
+    this->southWall = {getPlayArea().x - (getPlayArea().width/2), getPlayArea().y + getPlayArea().height, getPlayArea().width * 2, getPlayArea().height};
+    this->westWall = {getPlayArea().x - (getPlayArea().width/2), getPlayArea().y, getPlayArea().width/2, getPlayArea().height};
+    this->maxEnemies = 10;
+    this->maxProjectiles = 10;
+    this->initialEnemies = 3;
+    this->enemyRespawnTime = 600;
+    this->projectileRespawnTime = 600;
+    this->exitLocation = generateExitPosition();
+    this->enemiesArray = createEnemyArray();
+    this->projectilesArray = createProjectileArray();
+    // MIGHT POPULATE STAGE HERE
+}
+
+// getters
+Texture2D Stage::getSprite() 
+{
+    return this->sprite;
+}
+
+Rectangle Stage::getPlayArea() 
+{
+    return this->playArea;
+}
+
+Rectangle Stage::getNorthWall() 
+{
+    return this->northWall;
+}
+Rectangle Stage::getEastWall() 
+{
+    return this->eastWall;
+}
+Rectangle Stage::getSouthWall() 
+{
+    return this->southWall;
+}
+Rectangle Stage::getWestWall() 
+{
+    return this->westWall;
+}
+
+int Stage::getMaxEnemies() 
+{
+    return this->maxEnemies;
+}
+
+int Stage::getMaxProjectiles() 
+{
+    return this->maxProjectiles;
+}
+
+int Stage::getInitialEnemies() 
+{
+    return this->initialEnemies;
+}
+
+float Stage::getEnemyRespawnTime() 
+{
+    return this->enemyRespawnTime;
+}
+
+float Stage::getProjectileRespawnTime() 
+{
+    return this->projectileRespawnTime;
+}
+
+Vector2 Stage::getExitLocation() 
+{
+    return this->exitLocation;
+}
+
+Enemy* Stage::getEnemiesArray() 
+{
+    return this->enemiesArray;
+}
+
+Projectile* Stage::getProjectileArray() 
+{
+    return this->projectilesArray;
+}
+
+Player* Stage::getPlayerReference() 
+{
+    return this->playerReference;
+}
+
+// setters
+void Stage::setSprite(Texture2D sprite_) 
+{
+    this->sprite = sprite_;
+}
+
+void Stage::setPlayArea(Rectangle playArea_) 
+{
+    this->playArea = playArea_;
+}
+
+void Stage::setNorthWall(Rectangle northWall_) 
+{
+    this->northWall = northWall_;
+}
+
+void Stage::setEastWall(Rectangle eastWall_) 
+{
+    this->eastWall = eastWall_;
+}
+
+void Stage::setSouthWall(Rectangle southWall_) 
+{
+    this->southWall = southWall_;
+}
+
+void Stage::setWestWall(Rectangle westWall_) 
+{
+    this->westWall = westWall_;
+}
+
+void Stage::setMaxEnemies(int maxEnemies_) 
+{
+    this->maxEnemies = maxEnemies_;
+}
+
+void Stage::setMaxProjectiles(int maxProjectiles_) 
+{
+    this->maxProjectiles = maxProjectiles_;
+}
+
+void Stage::setInitialEnemies(int initialEnemies_) 
+{
+    this->initialEnemies = initialEnemies_;
+}
+
+void Stage::setEnemyRespawnTime(float enemyRespawnTime_) 
+{
+    this->enemyRespawnTime = enemyRespawnTime_;
+}
+
+void Stage::setProjectileRespawnTime(float projectileRespawnTime_) 
+{
+    this->projectileRespawnTime = projectileRespawnTime_;
+}
+
+void Stage::setExitLocation(Vector2 exitLocation_) 
+{
+    this->exitLocation = exitLocation_;
+}
+
+void Stage::setPlayerReference(Player* playerReference_) 
+{
+    this->playerReference = playerReference_;
+}
+
+// other
+void Stage::stageManager() 
+{
+
+}
+
+Enemy* Stage::createEnemyArray() 
+{
+
+}
+
+Projectile* Stage::createProjectileArray() 
+{
+
+}
+
+void Stage::initialPopulation() 
+{
+
+}
+
+Enemy Stage::spawnEnemy() 
+{
+
+}
+
+Projectile Stage::spawnProjectile() 
+{
+
+}
+
+void Stage::drawStage() 
+{
+
+}
+
+Vector2 Stage::generateExitPosition() 
+{
+
 }
--- classes/stage.hpp
+++ classes/stage.hpp
@@ -2,6 +2,9 @@
 #define stage_hpp
 
 #include "globals.hpp"
+#include "enemy.hpp"
+#include "projectile.hpp"
+#include "player.hpp"
 
 class Stage
 {
@@ -12,17 +15,66 @@ private:
     Rectangle eastWall; // eastWall
     Rectangle southWall; // southWall
     Rectangle westWall; // westWall
+    int maxEnemies;
+    int maxProjectiles;
+    int initialEnemies;
+    float enemyRespawnTime; // in frames
+    float projectileRespawnTime; // in frames
+    Vector2 exitLocation;
+    Enemy* enemiesArray; // dynamic array of enemies based on maxEnemies
+    Projectile* projectilesArray; // dynamic array of projectiles based on maxProjectiles
+    Player* playerReference; // reference to player object in order to get its position for spawning purposes
     
 public:
     // constructors
     Stage(); // Default constructor
-    Stage(Texture2D, Rectangle); // Constructor with parameters
+    Stage(Texture2D, Rectangle, Player*); // Constructor with parameters
 
     // getters
+    Texture2D getSprite();
+    Rectangle getPlayArea();
+    Rectangle getNorthWall();
+    Rectangle getEastWall();
+    Rectangle getSouthWall();
+    Rectangle getWestWall();
+    int getMaxEnemies();
+    int getMaxProjectiles();
+    int getInitialEnemies();
+    float getEnemyRespawnTime();
+    float getProjectileRespawnTime();
+    Vector2 getExitLocation();
+    Enemy* getEnemiesArray();
+    Projectile* getProjectileArray();
+    Player* getPlayerReference();
 
     // setters
-    void setSprite(Texture2D); // sets sprite
-    void setPlayArea(Rectangle); // sets playArea
+    void setSprite(Texture2D);
+    void setPlayArea(Rectangle);
+    void setNorthWall(Rectangle);
+    void setEastWall(Rectangle);
+    void setSouthWall(Rectangle);
+    void setWestWall(Rectangle);
+    void setMaxEnemies(int);
+    void setMaxProjectiles(int);
+    void setInitialEnemies(int);
+    void setEnemyRespawnTime(float);
+    void setProjectileRespawnTime(float);
+    void setExitLocation(Vector2);
+    void setPlayerReference(Player*);
+
+    // other
+    void stageManager(); // master controller, called once per frame, processes entire stage (& respawns)
+    Enemy* createEnemyArray(); // allocates memory for enemy array to keep track of all enemy objects
+    Projectile* createProjectileArray(); // allocates memory for proj. array to track all projectiles
+    void initialPopulation(); // creates enemies and puts them in array based on initialEnemies
+    Enemy spawnEnemy(); // creates an enemy and puts it in the array (check for space done in manager)
+    Projectile spawnProjectile(); // creates a proj. and puts in array (check for space done in manager)
+    void drawStage(); // draws walls and background
+    Vector2 generateExitPosition(); // generates the position of the exit
+
+    // Destructor
+    ~Stage();
+
 };
 
 #endif // stage_hpp
