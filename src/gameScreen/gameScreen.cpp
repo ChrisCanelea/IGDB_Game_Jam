9aa1e53715751d15f30a1d552403f97a4116d22d
ChrisCanelea <chris.canelea@gmail.com>
2023-09-07 20:24:46 -0400

    camera work kinda

--- src/gameScreen/gameScreen.cpp
+++ src/gameScreen/gameScreen.cpp
@@ -5,17 +5,13 @@
 void gameScreen(void)
 {
     Player player;
-    Camera2D camera = {{0,0}, {0,0}, 0.0f, 0.5f}; // camera initialization
-    Vector2 cameraPos = {SCREEN_W/2, SCREEN_H/2};
-    Vector2 cameraLerp;
+    Camera2D camera = {{SCREEN_W/2, SCREEN_H/2}, {0,0}, 0.0f, 1.0f}; // camera initialization
     player.setHeight(50);
     player.setWidth(50);
     while(true)
     {
-        cameraLerp = Vector2Lerp(cameraPos, player.getPos(), 0.15);
-        camera.target = cameraLerp;
-        cameraPos.x += cameraLerp.x;
-        cameraPos.y += cameraLerp.y;
+        camera.target = player.getPos();
+        // Vector2Lerp(camera.offset, player.getPos(), 0.15);
         
         BeginDrawing();
 
