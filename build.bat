6e860317c4b6cc1e2da87630a35a072440e9d02d
ssergnese83 <ssergnese83@gmail.com>
2023-09-07 20:11:02 -0400

    fix

--- build.bat
+++ build.bat
@@ -1,2 +1,1 @@
 g++ src/*.cpp src/titleScreen/*.cpp src/gameScreen/*.cpp classes/*.cpp -Wl,--subsystem,windows -mwindows -o game -Wall -I include -I classes -L lib -lraylib -lopengl32 -lgdi32 -lwinmm
-@REM test
